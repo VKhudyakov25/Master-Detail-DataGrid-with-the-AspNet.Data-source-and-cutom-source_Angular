95aced42765ecea854f88fed648d3ede52c61f95
Vyacheslav Khudyakov <slavak521@MacBook-Air-Vyacheslav.local>
2023-02-28 00:32:05 +0600

    Implement pre-complited task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { createStore } from 'devextreme-aspnet-data-nojquery';
+// Need an assistance on this task. All detail dataGrid components is re-render after updating OrderID.
+// I think the problem in Change Detection.
+
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import CustomStore from 'devextreme/data/custom_store';
+import DataSource from 'devextreme/data/data_source';
 
 import { AppService } from './app.service';
 
@@ -8,18 +12,54 @@ import { AppService } from './app.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [AppService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  orderId: any;
   store: any;
+  dataSource: DataSource;
+  detailDataSource: DataSource;
+  controlDataSource: any;
 
-  constructor() {
-    let serviceUrl = 'https://js.devexpress.com/Demos/Mvc/api/DataGridWebApi';
-    this.store = createStore({
-      key: 'OrderID',
-      loadUrl: serviceUrl + '/Orders',
-      onBeforeSend: (method, ajaxOptions) => {
-        ajaxOptions.xhrFields = { withCredentials: true };
-      },
+  changeValue(value: any) {
+    console.log(value);
+    this.orderId = value.key;
+  }
+
+  constructor(service: AppService) {
+    this.dataSource = new DataSource({
+      store: new CustomStore({
+        key: 'OrderID',
+        load: () => {
+          return service.getOrders();
+        },
+        insert: (val) => {
+          return service.insertOrder(val);
+        },
+        update: (key, val) => {
+          return service.updateOrder(key, val);
+        },
+        remove: (key: number): any => {
+          return service.removeOrder(key);
+        },
+      }),
+    });
+    this.detailDataSource = new DataSource({
+      store: new CustomStore({
+        load: () => {
+          console.log(this.orderId);
+          return service.getDetails(this.orderId);
+        },
+        insert: (val) => {
+          return service.insertDetails(val, this.orderId);
+        },
+        update: (key, val) => {
+          return service.updateDetails(key, val, this.orderId);
+        },
+        remove: (key): any => {
+          return service.removeDetails(key, this.orderId);
+        },
+      }),
     });
   }
 }
--- src/app/detail-grid/detail-grid.component.ts
+++ src/app/detail-grid/detail-grid.component.ts
@@ -1,6 +1,8 @@
+import DataSource from 'devextreme/data/data_source';
 import { AfterViewInit, Component, Input } from '@angular/core';
 import { createStore } from 'devextreme-aspnet-data-nojquery';
 import { AppService } from '../app.service';
+import CustomStore from 'devextreme/data/custom_store';
 
 @Component({
   selector: 'app-detail-grid',
@@ -8,22 +10,28 @@ import { AppService } from '../app.service';
   styleUrls: ['./detail-grid.component.css'],
   providers: [AppService],
 })
-export class DetailGridComponent implements AfterViewInit {
+export class DetailGridComponent {
   @Input() orderId!: number;
 
-  detailDataSource: any;
+  detailDataSource: DataSource;
 
-  constructor() {}
-
-  ngAfterViewInit(): void {
-    console.log(this.orderId);
-    let serviceUrl = 'https://js.devexpress.com/Demos/Mvc/api/DataGridWebApi';
-    this.detailDataSource = createStore({
-      loadUrl: serviceUrl + '/OrderDetails',
-      loadParams: { orderId: this.orderId },
-      onBeforeSend(method: any, ajaxOptions: any) {
-        ajaxOptions.xhrFields = { withCredentials: true };
-      },
+  constructor(service: AppService) {
+    this.detailDataSource = new DataSource({
+      store: new CustomStore({
+        load: () => {
+          console.log(this.orderId);
+          return service.getDetails(this.orderId);
+        },
+        insert: (val) => {
+          return service.insertDetails(val, this.orderId);
+        },
+        update: (key, val) => {
+          return service.updateDetails(key, val, this.orderId);
+        },
+        remove: (key): any => {
+          return service.removeDetails(key, this.orderId);
+        },
+      }),
     });
   }
 }
